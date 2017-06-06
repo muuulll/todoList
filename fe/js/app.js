190c901dee47b3acd2adfe657c579a0e309d4020
muuulll <kimmuuulll@gmail.com>
2017-06-06 13:50:45 +0900

    Update app.js

--- fe/js/app.js
+++ fe/js/app.js
@@ -1,6 +1,42 @@
 (function (window) {
-	'use strict';
+	
+	function findAll(){
+	    $.ajax({
+            url:'./api/todos/hello',
+            success:function(data){
+                $('#onetodo').append(data);
+            }
+	    });
+	}
 
-	// Your starting point. Enjoy the ride!
+	function save(){
+		alert('ok!');
+	}
+
+	findAll();
+	save();
 
 })(window);
+
+
+function create(){
+	if(event.keyCode == 13){
+        $.ajax({
+            url:'./api/todos',
+            dataType:'json',
+            type:'post',
+            data:$('form').serialize(),
+            success:function(data){
+                $('#onetodo').text(data);
+            }
+        })
+    }
+}
+
+function active(){
+	alert("active");
+}
+
+function completed(){
+	alert("completed");
+}
